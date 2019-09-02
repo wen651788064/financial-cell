69759c156b5c1bcfbd6c02a0129e8988a1a24265
651788064@qq.com <651788064@qq.com>
2019-09-02 13:29:28 +0800

    a

--- src/component/table.js
+++ src/component/table.js
@@ -89,7 +89,7 @@ export function parseCell(viewRange, state = false, src = '') {
     });
 
     if(state) {
-        workbook.Sheets.Sheet1['A1'] = {v: src, f: ""};
+        workbook.Sheets.Sheet1['A1'] = {v: `=${src}`, f: ""};
     }
 
     try {
