2b20cd2899c6cec8d7386de1c38919c1508433f4
651788064@qq.com <651788064@qq.com>
2019-09-09 15:53:02 +0800

    update

--- src/component/cell_proxy.js
+++ src/component/cell_proxy.js
@@ -22,38 +22,42 @@ export default class CellProxy {
         Object.keys(newData).forEach(i => {
             Object.keys(newData[i]).forEach(j => {
                 if(j == name) {
-                    Object.keys(newData[i][j]).forEach(k => {
-                        let newCell = newData[i][j][k];
-                        let oldCell = oldData[i][j][k];
-                        if (oldCell.v != undefined && newCell.v != undefined && newCell.v + "" && oldCell.v + "" && newCell.v + "" !== oldCell.v + "") {
-                            let expr = k;
-                            newCell.v = newCell.v + "";
-                            if (!isNaN(newCell.v.replace(/ /g, '').toUpperCase().replace(/\"/g, "\""))) {
-                                workbook.Sheets[name][expr] = {
-                                    v: newCell.v.replace(/ /g, '').toUpperCase().replace(/\"/g, "\"") * 1,
-                                };
-                            } else {
-                                workbook.Sheets[name][expr] = {
-                                    v: newCell.v.replace(/ /g, '').toUpperCase().replace(/\"/g, "\""),
-                                };
-                            }
-                        } else if (oldCell.f != undefined && newCell.f != undefined && newCell.f + "" && oldCell.f + "" && newCell.f + "" !== oldCell.f + "") {
-                            let expr = k;
-                            newCell.f = newCell.f + "";
-                            if(newCell.f && newCell.f[0] === "=" && isSheetVale(newCell.f)) {
-                                deep.push(newCell.f);
-                            }
+                    try {
+                        Object.keys(newData[i][j]).forEach(k => {
+                            let newCell = newData[i][j][k];
+                            let oldCell = oldData[i][j][k];
+                            if (oldCell.v != undefined && newCell.v != undefined && newCell.v + "" && oldCell.v + "" && newCell.v + "" !== oldCell.v + "") {
+                                let expr = k;
+                                newCell.v = newCell.v + "";
+                                if (!isNaN(newCell.v.replace(/ /g, '').toUpperCase().replace(/\"/g, "\""))) {
+                                    workbook.Sheets[name][expr] = {
+                                        v: newCell.v.replace(/ /g, '').toUpperCase().replace(/\"/g, "\"") * 1,
+                                    };
+                                } else {
+                                    workbook.Sheets[name][expr] = {
+                                        v: newCell.v.replace(/ /g, '').toUpperCase().replace(/\"/g, "\""),
+                                    };
+                                }
+                            } else if (oldCell.f != undefined && newCell.f != undefined && newCell.f + "" && oldCell.f + "" && newCell.f + "" !== oldCell.f + "") {
+                                let expr = k;
+                                newCell.f = newCell.f + "";
+                                if(newCell.f && newCell.f[0] === "=" && isSheetVale(newCell.f)) {
+                                    deep.push(newCell.f);
+                                }
 
-                            if (isNaN(newCell.f)) {
-                                newCell.f = newCell.f.toUpperCase();
-                            }
+                                if (isNaN(newCell.f)) {
+                                    newCell.f = newCell.f.toUpperCase();
+                                }
 
-                            workbook.Sheets[name][expr] = {
-                                v: '',
-                                f: newCell.f.replace(/ /g, '').replace(/\"/g, "\"").replace(/\"\"\"\"&/g, "\"'\"&")
-                            };
-                        }
-                    });
+                                workbook.Sheets[name][expr] = {
+                                    v: '',
+                                    f: newCell.f.replace(/ /g, '').replace(/\"/g, "\"").replace(/\"\"\"\"&/g, "\"'\"&")
+                                };
+                            }
+                        });
+                    } catch (e) {
+                        console.log(e);
+                    }
                 }
             });
         });
