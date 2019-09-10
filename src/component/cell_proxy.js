f359185df69db747d949f92f3bed1264a2a77082
651788064@qq.com <651788064@qq.com>
2019-09-10 15:58:47 +0800

    update

--- src/component/cell_proxy.js
+++ src/component/cell_proxy.js
@@ -122,7 +122,7 @@ export default class CellProxy {
         if (typeof this.oldData == "string") {
             this.oldData = this.deepCopy(newData);
             return {
-                "state": true,
+                "state": false,
                 "data": this.oldData.Sheets[name],
             };
         }
