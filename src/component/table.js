619aed8d9fa6382cff84f9488640388f919a8f16
651788064@qq.com <651788064@qq.com>
2019-09-10 17:24:58 +0800

    update

--- src/component/table.js
+++ src/component/table.js
@@ -141,10 +141,10 @@ async function parseCell(viewRange, state = false, src = '') {
 
     let sall = loadData.call(this, viewRange, false, true).workbook;
     Object.keys(s).forEach(i => {
-        if(i !== data.name) {
+        // if(i !== data.name) {
             workbook.Sheets[i] = s[i];
             sall.Sheets[i] = s[i];
-        }
+        // }
     });
     let ca = proxy.calc(sall, data.name);
     if (ca.state) {
