d84f9e5744448fe9279a0436fa3bc41a0a8af26a
651788064@qq.com <651788064@qq.com>
2019-09-16 11:45:51 +0800

    update

--- src/component/cell_proxy.js
+++ src/component/cell_proxy.js
@@ -159,7 +159,7 @@ export default class CellProxy {
         // let {worker} = this;
         for (let j = 0; j < rr.length; j++) {
             let recordArr = [];
-            let data = this.deepCopy(rr[i].workbook);
+            let data = this.deepCopy(rr[j].workbook);
             data = this.filter(data);
 
             for (let i = 0; i < tileArr.length; i++) {
